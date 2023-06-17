8d92fb17ff3b146e78aa1e00045f25c5f51475d3
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-06-17 14:34:53 +0800

    Update get_time_greeting.php

--- get_time_greeting.php
+++ get_time_greeting.php
@@ -5,13 +5,13 @@ date_default_timezone_set('Asia/Kuala_Lumpur');
 $Hour = date('G');
 $json = array();
 if ( $Hour >= 5 && $Hour <= 11 ) {
-    $json = array('text' => 'Good morning');
+    $json = array('greet' => 'Good morning');
 } else if ( $Hour >= 12 && $Hour <= 18 ) {
-    $json = array('text' => 'Good afternoon');
+    $json = array('greet' => 'Good afternoon');
 } else if ( $Hour >= 19 || $Hour <= 23 ) {
-    $json = array('text' => 'Good evening');
+    $json = array('greet' => 'Good evening');
 } else if ( $Hour >= 0 || $Hour <= 4 ) {
-    $json = array('text' => 'Hello night owl');
+    $json = array('greet' => 'Hello night owl');
 }
 echo json_encode($json);
 ?>
