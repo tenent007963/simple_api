9b8605c4e4990449f522c40b4100bae832f0c9e2
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-06-17 14:25:52 +0800

    Update get_time_greeting.php

--- get_time_greeting.php
+++ get_time_greeting.php
@@ -1,17 +1,17 @@
 <?php
-// I'm India so my timezone is Asia/Calcutta
 date_default_timezone_set('Asia/Kuala_Lumpur');
 
 // 24-hour format of an hour without leading zeros (0 through 23)
 $Hour = date('G');
-
+$json = array();
 if ( $Hour >= 5 && $Hour <= 11 ) {
-    echo "Good Morning";
+    $json = array('text' => 'Good morning');
 } else if ( $Hour >= 12 && $Hour <= 18 ) {
-    echo "Good Afternoon";
+    $json = array('text' => 'Good afternoon');
 } else if ( $Hour >= 19 || $Hour <= 23 ) {
-    echo "Good Evening";
+    $json = array('text' => 'Good evening');
 } else if ( $Hour >= 0 || $Hour <= 4 ) {
-    echo "Hello night owl";
+    $json = array('text' => 'Hello night owl');
 }
+echo json_encode($json);
 ?>
